ed7e3f4b055c216009e68c5a4cfc9025424574ad
devWorld335 <ajeerony1111@gmail.com>
2026-01-11 01:59:39 -0500

    feat: add moniter

---
+++ services/tradeMonitor.ts
@@ -0,0 +1,20 @@
+import WebSocket from 'ws';
+import { ENV } from '../config/env';
+import { getUserActivityModel, getUserPositionModel } from '../models/userHistory';
+import fetchData from '../utils/fetchData';
+import Logger from '../utils/logger';
+
+const USER_ADDRESSES = ENV.USER_ADDRESSES;
+const TOO_OLD_TIMESTAMP = ENV.TOO_OLD_TIMESTAMP;
+const RTDS_URL = ENV.RTDS_URL;
+
+if (!USER_ADDRESSES || USER_ADDRESSES.length === 0) {
+    throw new Error('USER_ADDRESSES is not defined or empty');
+}
+
+// Create activity and position models for each user
+const userModels = USER_ADDRESSES.map((address) => ({
+    address,
+    UserActivity: getUserActivityModel(address),
+    UserPosition: getUserPositionModel(address),
+}));
